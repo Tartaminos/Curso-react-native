1ea74e285c0251f025121fc015cce7ddde6d8572
Johann <johannsimon@hotmail.com>
2022-01-07 22:19:33 -0300

    Organização dos componentes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { View } from 'react-native'
+
+import First from './components/First'
+
+
+export default () => (
+        <View>
+            <First />   
+        </View>
+)
