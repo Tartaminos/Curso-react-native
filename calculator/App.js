f5c2f3fbba64ce14ace52d1afafa557ca6c11409
Johann <johannsimon@hotmail.com>
2022-02-08 21:59:38 -0300

    implementando os botões clicáveis

--- calculator/App.js
+++ calculator/App.js
@@ -3,37 +3,56 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Component } from 'react/cjs/react.production.min';
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 export default class App extends Component {
+  state = {
+    displayValue: '0',
+    oi: 0
+  }
+
+  addDigit = n => {
+    this.setState({displayValue: n})
+  }
+
+  clearMemory = () => {
+    this.setState({displayValue: '0'})
+  }
+
+  setOperation = operation => {
+    this.setState({displayValue: operation})
+  }
+
   render(){
     return (
+      <View style={styles.sectionContainer}>
+        <Display value={this.state.displayValue} /> 
         <View style={styles.buttons}>
-          <Button label='AC' />
-          <Button label='/' />
-          <Button label='7' />
-          <Button label='8' />
-          <Button label='9' />
-          <Button label='*' />
-          <Button label='4' />
-          <Button label='5' />
-          <Button label='6' />
-          <Button label='-' />
-          <Button label='1' />
-          <Button label='2' />
-          <Button label='3' />
-          <Button label='+' />
-          <Button label='0' />
-          <Button label='.' />
-          <Button label='=' />
+          <Button label='AC' triple onClick={this.clearMemory} />
+          <Button label='/' operation onClick={this.setOperation} />
+          <Button label='7' onClick={this.addDigit} />
+          <Button label='8' onClick={this.addDigit} />
+          <Button label='9' onClick={this.addDigit} />
+          <Button label='*' operation onClick={this.setOperation} />
+          <Button label='4' onClick={this.addDigit} />
+          <Button label='5' onClick={this.addDigit} />
+          <Button label='6' onClick={this.addDigit} />
+          <Button label='-' operation onClick={this.setOperation} />
+          <Button label='1' onClick={this.addDigit} />
+          <Button label='2' onClick={this.addDigit} />
+          <Button label='3' onClick={this.addDigit} />
+          <Button label='+' operation onClick={this.setOperation} />
+          <Button label='0' double onClick={this.addDigit} />
+          <Button label='.' operation onClick={this.setOperation} />
+          <Button label='=' operation onClick={this.setOperation} />
         </View>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
   },
   buttons: {
     flexDirection: 'row',
